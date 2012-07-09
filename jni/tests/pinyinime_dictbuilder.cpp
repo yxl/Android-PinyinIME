37bd0cfffdd452b4871ba2c35b5a5a78c176b7ce
Yuan Xulei <xyuan@mozilla.com>
2012-07-09 10:15:52 +0800

    add testing code for prediction.

--- jni/tests/pinyinime_dictbuilder.cpp
+++ jni/tests/pinyinime_dictbuilder.cpp
@@ -26,11 +26,10 @@
 
 using namespace ime_pinyin;
 
-UTF8 g_utf8_buf[1024] =
-{ 0 };
+UTF8 g_utf8_buf[1024] = { 0 };
 
-const char* toUTF8(const char16* src, size_t length)
-{
+const char*
+toUTF8(const char16* src, size_t length) {
   UTF16 *utf16Start = (UTF16 *) src;
   UTF16 *utf16End = (UTF16 *) (src + length);
   UTF8* utf8Start = g_utf8_buf;
@@ -39,31 +38,40 @@ const char* toUTF8(const char16* src, size_t length)
   return (const char*) g_utf8_buf;
 }
 
-void testMatrixSearch(const char* py)
-{
+void
+testMatrixSearch(const char* py) {
   MatrixSearch ms;
   ms.init("../../res/raw/dict_pinyin.png", "user.dic");
-  for (int i=0; i<10; i++) {
-  ms.reset_search();
-  ms.search(py, strlen(py));
+  for (int i = 0; i < 10; i++) {
+    ms.reset_search();
+    ms.search(py, strlen(py));
 
-  char16 buffer[64];
+    char16 buffer[64];
 
-  ms.get_candidate0(buffer, 64, NULL, false);
+    ms.get_candidate0(buffer, 64, NULL, false);
 
-  size_t n = ms.get_candidate_num();
+    size_t n = ms.get_candidate_num();
 
-  printf("\n");
-  for (size_t i = 0; i < n; i++)
-  {
-    if (i % 10 == 0)
-    {
-      printf("\n");
+    printf("\n");
+    for (size_t i = 0; i < n; i++) {
+      if (i % 10 == 0) {
+        printf("\n");
+      }
+      ms.get_candidate(i, buffer, 64);
+      printf("%s ", toUTF8(buffer, 64));
     }
-    ms.get_candidate(i, buffer, 64);
-    printf("%s ", toUTF8(buffer, 64));
-  }
-  printf("\n%ld candidate(s) for %s\n", n, py);
+    printf("\n%ld candidate(s) for %s\n", n, py);
+
+    char16 predict_buf[100][kMaxPredictSize + 1];
+    ms.get_candidate0(buffer, 64, NULL, false);
+    n = ms.get_predicts(buffer, predict_buf, 100);
+    printf("\n%ld prediction word(s) for %s\n", n, toUTF8(buffer, 64));
+    for (size_t i = 0; i < n; i++) {
+      if (i % 10 == 0) {
+        printf("\n");
+      }
+      printf("%s ", toUTF8(predict_buf[i], kMaxPredictSize + 1));
+    }
   }
 }
 
@@ -71,11 +79,10 @@ void testMatrixSearch(const char* py)
  * Build binary dictionary model. Make sure that ___BUILD_MODEL___ is defined
  * in dictdef.h.
  */
-int main(int argc, char* argv[])
-{
+int
+main(int argc, char* argv[]) {
   char py[256] = "nia";
-  if (argc > 1)
-  {
+  if (argc > 1) {
     strncpy(py, argv[1], 256);
   }
   testMatrixSearch(py);
