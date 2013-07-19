b3a4bb44d639f2e47a59fdc1f4b943d563efbbd0
pzhang <pzhang@mozilla.com>
2013-07-19 17:05:56 +0800

    Update build.sh and test.html

--- jni/empinyin/build.sh
+++ jni/empinyin/build.sh
@@ -1,7 +1,26 @@
 # !/bin/sh
 
 echo "Build libpinyin.js"
-emcc ../share/pinyinime.cpp ../share/dictbuilder.cpp ../share/dicttrie.cpp ../share/matrixsearch.cpp ../share/ngram.cpp ../share/searchutility.cpp ../share/spellingtrie.cpp ../share/sync.cpp ../share/utf16char.cpp ../share/dictlist.cpp ../share/lpicache.cpp ../share/mystdlib.cpp ../share/spellingtable.cpp ../share/splparser.cpp ../share/userdict.cpp ../share/utf16reader.cpp -O2 -g3 --closure 0 -o libpinyin.js -s LINKABLE=1 -s TOTAL_STACK=262144 -s TOTAL_MEMORY=16777216 -s EXPORTED_FUNCTIONS="['_im_open_decoder', '_im_open_decoder_fd', '_im_close_decoder', '_im_set_max_lens', '_im_flush_cache', '_im_search', '_im_delsearch', '_im_reset_search', '_im_get_sps_str', '_im_get_candidate', '_im_get_spl_start_pos', '_im_choose', '_im_cancel_last_choice', '_im_get_fixed_len', '_im_get_predicts', '_im_enable_shm_as_szm', '_im_enable_ym_as_szm']"
+emcc ../share/pinyinime.cpp ../share/dictbuilder.cpp ../share/dicttrie.cpp \
+     ../share/matrixsearch.cpp ../share/ngram.cpp ../share/searchutility.cpp \
+     ../share/spellingtrie.cpp ../share/sync.cpp ../share/utf16char.cpp \
+     ../share/dictlist.cpp ../share/lpicache.cpp ../share/mystdlib.cpp \
+     ../share/spellingtable.cpp ../share/splparser.cpp ../share/userdict.cpp \
+     ../share/utf16reader.cpp -O2 --closure 0 -g3 -o libpinyin.js -s LINKABLE=1 \
+     -s ASM_JS=1 -s TOTAL_STACK=262144 -s TOTAL_MEMORY=16777216 -s EXPORTED_FUNCTIONS="['_im_open_decoder', \
+     '_im_open_decoder_fd', '_im_close_decoder', '_im_set_max_lens', '_im_flush_cache', \
+     '_im_search', '_im_delsearch', '_im_reset_search', '_im_get_sps_str', '_im_get_candidate', \
+     '_im_get_spl_start_pos', '_im_choose', '_im_cancel_last_choice', '_im_get_fixed_len', \
+     '_im_get_predicts', '_im_enable_shm_as_szm', '_im_enable_ym_as_szm', '_malloc']"
+
+echo "Entering ../command "
+cd ../command
+
+make
+./pinyinime_dictbuilder
+
+echo "Entering ../empinyin"
+cd ../empinyin
 
 mkdir data
 
