4473fb7e332d2e4b147eac273d45720624638c5a
pzhang <pzhang@mozilla.com>
2013-07-25 14:02:11 +0800

    Add script to batch run candidates searching

---
+++ jni/tests/run_tests.sh
@@ -0,0 +1,13 @@
+# !/bin/sh
+
+echo "Make ..."
+make
+
+echo "Run tests .."
+LIST="a b c d e f g h z ba da fa ga ha"
+for i in $LIST
+do
+  echo "run 10000 times for $i:"
+  ./pinyinime_dictbuilder $i
+done
+
